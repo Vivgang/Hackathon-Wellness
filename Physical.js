0946a2a3e36037fd645dd2656ec14d98b4751f61
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2023-03-04 15:21:58 -0600

    Update Physical.js

--- Physical.js
+++ Physical.js
@@ -1,1 +1,10 @@
 
+function submit() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length; i++) {
+        text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("correct").innerHTML = "Is this correct?(order of question)<br>" + text + "If something doesn't apply to you, change it and resubmit" ;
+}
