5ff78cb5005f88df9e5981c5af67ffee42e42f85
POTATORGOOD <101843193+POTATORGOOD@users.noreply.github.com>
2023-03-04 19:35:52 -0600

    Update Physical.js

--- Physical.js
+++ Physical.js
@@ -1,9 +1,62 @@
+var x = document.getElementById("frm1");
 function submit() {
-    var x = document.getElementById("frm1");
+   
     var text = "";
     var i;
     for (i = 0; i < x.length; i++) {
         text += x.elements[i].value + "<br>";
     }
-    document.getElementById("correct").innerHTML = "Is this correct?(order of question)<br>" + text + "If something doesn't apply to you, change it and resubmit";
+    document.getElementById("correct").innerHTML = "Is this correct?(order of question)<br>" + text + "If something doesn't apply to you, change it and resubmit" ;
+}
+var fail = false;
+
+function confirm() {
+    
+    if (document.forms["frm1"]["0"].value == "") {
+        alert("All info must be filled out");
+        fail = false;
+    }
+    else {
+        if (document.forms["frm1"]["1"].value == "") {
+            alert("All info must be filled out");
+            fail = false;
+        }
+        else {
+            if (document.forms["frm1"]["2"].value == "") {
+                alert("All info must be filled out");
+                fail = false;
+            }
+            else {
+                if (document.forms["frm1"]["3"].value == "") {
+                    alert("All info must be filled out");
+                    fail = false;
+                }
+                else {
+                    if (document.forms["frm1"]["4"].value == "") {
+                        alert("All info must be filled out");
+                        fail = false;
+                    }
+                    else {
+                        if (document.forms["frm1"]["5"].value == "") {
+                            alert("All info must be filled out");
+                            fail = false;
+                        }
+                        else {
+                            if (document.forms["frm1"]["6"].value == "") {
+                                alert("All info must be filled out");
+                                fail = false;
+                            }
+                            else {
+                                fail = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+if (fail == true) {
+
 }
